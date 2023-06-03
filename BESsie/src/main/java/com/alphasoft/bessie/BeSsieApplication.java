269136e711e2d58c624c7a60a032f689d7608cb9
Oleksiy Blavat <oblavat@invoca.com>
2023-06-03 16:05:44 -0400

    blank springboot project for websocket server

---
+++ BESsie/src/main/java/com/alphasoft/bessie/BeSsieApplication.java
@@ -0,0 +1,13 @@
+package com.alphasoft.bessie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeSsieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeSsieApplication.class, args);
+    }
+
+}
