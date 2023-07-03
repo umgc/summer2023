bb36d0207bb31046ac1cc71348fc4e555e6c922a
Charlie McAllister <cmcallister8@student.umgc.edu>
2023-07-02 23:24:03 -0400

    Form fill logic is working

--- form-filler-extension/js/contentScript.js
+++ form-filler-extension/js/contentScript.js
@@ -4,11 +4,19 @@ chrome.runtime.onMessage.addListener((obj, sender, sendResponse) => {
     if (mode === 'FORM_SCRAPE') {
         // Collect all input elements and put in a list of name : type
         let inputElements = document.getElementsByTagName("input");
+        let textAreaInputs = document.getElementsByTagName("textarea");
+        let allInputs = [];
+        allInputs.push.apply(allInputs, inputElements);
+        allInputs.push.apply(allInputs, textAreaInputs);
+
         let formFields = [];
-        for (const inputElement of inputElements) {
+        for (const inputElement of allInputs) {
             if (inputElement.type === 'hidden' 
                 || inputElement.type === 'submit' 
-                || inputElement.type === 'button') {
+                || inputElement.type === 'button'
+                || inputElement.type === 'radio'
+                || inputElement.type === 'checkbox'
+                || inputElement.type === 'file') {
                 continue;
             }
             fieldEntry = {};
@@ -18,6 +26,12 @@ chrome.runtime.onMessage.addListener((obj, sender, sendResponse) => {
         sendResponse({"formFields": formFields});
         return true;
     } else if (mode === 'FORM_FILL') {
-        console.log("Form values from BESie -> " + data);
+        console.log("Form values from BESie -> " + data.payload);
+        for (const key of Object.keys(data.payload)) {
+            const formElement = document.getElementsByName(key)[0];
+            if (formElement) {
+                formElement.value = data.payload[key];
+            }
+        }
     }
 });
