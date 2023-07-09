aeb762d75c5e4cfb342175c5c976528541ebf5fb
JonathanNagy <jonathan.a.nagy@gmail.com>
2023-07-09 18:57:09 -0400

    check rootContext is mounted before displaying dialog

--- talker-mobile-app/lib/services/DummyConversationSelectionActivator.dart
+++ talker-mobile-app/lib/services/DummyConversationSelectionActivator.dart
@@ -4,9 +4,10 @@ import 'package:flutter/material.dart';
 import 'package:panara_dialogs/panara_dialogs.dart';
 import 'package:talker_mobile_app/globals.dart';
 
-class DummyConversationSelectionActivator implements RecordingSelectionActivator {
+class DummyConversationSelectionActivator
+    implements RecordingSelectionActivator {
   DummyConversationSelectionActivator(this.rootContext);
-  
+
   bool didCallSelector = false;
   BuildContext rootContext;
 
@@ -14,23 +15,25 @@ class DummyConversationSelectionActivator implements RecordingSelectionActivator
   RecordingSelectionActivatorCallback getSelectorCallback() {
     return () async {
       didCallSelector = true;
-      
+
+      // Ensure context is still mounted before displaying dialog
+      if (!rootContext.mounted) return;
       PanaraInfoDialog.show(
-          rootContext, 
-          title: "Dummy Selection",
-          message: "This is where the conversation selection should happen.",
-          buttonText: "Okay",
-          onTapDismiss: () async {
-            // Backend services needs to finish integrating with the recordings store managed 
-            // by ConvoBuddy and applying transcripts back to the recording metadata. Once that
-            // is done, this call will take a recording guid and call ChatGPT with the transcript
-            // and forward the results to the browser extension via BESie.
-            // var recordingGuid = 'some-recording-guid';
-            // await getIt<Agent>().extractFormValues(recordingGuid);
-            Navigator.pop(rootContext);
-          },
-          panaraDialogType: PanaraDialogType.normal,
-          barrierDismissible: false, 
+        rootContext,
+        title: "Dummy Selection",
+        message: "This is where the conversation selection should happen.",
+        buttonText: "Okay",
+        onTapDismiss: () async {
+          // Backend services needs to finish integrating with the recordings store managed
+          // by ConvoBuddy and applying transcripts back to the recording metadata. Once that
+          // is done, this call will take a recording guid and call ChatGPT with the transcript
+          // and forward the results to the browser extension via BESie.
+          // var recordingGuid = 'some-recording-guid';
+          // await getIt<Agent>().extractFormValues(recordingGuid);
+          Navigator.pop(rootContext);
+        },
+        panaraDialogType: PanaraDialogType.normal,
+        barrierDismissible: false,
       );
     };
   }
