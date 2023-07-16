fdd86fbcec200f1f9708ccdcbad61c32c5081148
JonathanNagy <jonathan.a.nagy@gmail.com>
2023-07-16 09:06:59 -0400

    updated form filler int_test, remove generated files

--- backend_test_utility/integration_test/gpt_code_test.dart
+++ backend_test_utility/integration_test/gpt_code_test.dart
@@ -26,19 +26,65 @@ void main() async {
 
   test('Send transcript and form values to OpenAI for form fill', () async {
     final conversation = TestConversations.sampleConversations.firstWhereOrNull(
-        (convo) => convo.id == '173d6dc0-fb47-4284-bd09-9465177f8eea');
+        (convo) => convo.id == 'cff13755-0a90-4f90-975f-fe9f5a13fcd5');
 
     expect(conversation, isNotNull);
     expect(conversation!.transcript, isNotEmpty);
 
     final formFields = [
-      "firstName",
-      "lastName",
-      "email",
-      "address",
-      "city",
-      "state",
-      "zip"
+      [
+        {"firstNameField": "text"},
+        {"lastNameField": "text"},
+        {"emailField": "text"},
+        {"country": "tel"},
+        {"input-7": "text"},
+        {
+          "select-di-01-0": {
+            "inputType": "select",
+            "optionList": [
+              "",
+              "Undergraduate/Bachelors",
+              "Graduate/Master's",
+              "Doctorate",
+              "Not Sure"
+            ]
+          }
+        },
+        {
+          "select-02-0": {
+            "inputType": "select",
+            "optionList": [
+              "",
+              "Business & Management",
+              "Cybersecurity",
+              "Data Analytics",
+              "Education & Teaching",
+              "Health Care & Sciences",
+              "Information Technology",
+              "Liberal Arts & Communication",
+              "Psychology",
+              "Public Safety & Criminal Justice",
+              "Not Sure/Other"
+            ]
+          }
+        },
+        {
+          "select-03-0": {
+            "inputType": "select",
+            "optionList": [
+              "",
+              "Active-duty servicemember",
+              "Spouse of active-duty servicemember",
+              "Family member of active-duty servicemember",
+              "Veteran",
+              "National Guard",
+              "Reservist",
+              "Other",
+              "None / Civilian"
+            ]
+          }
+        }
+      ],
     ];
 
     // send to chatgpt
@@ -50,7 +96,7 @@ void main() async {
     logger.i(completion);
   });
 
-    test('Send transcript to OpenAI for Restaurant Order', () async {
+  test('Send transcript to OpenAI for Restaurant Order', () async {
     final conversation = TestConversations.sampleConversations.firstWhereOrNull(
         (convo) => convo.id == '0a60bf00-a057-4ad7-87fd-534c7d407160');
     final gpt = GptCalls(EnvironmentVars.openAIApiKey);
@@ -62,15 +108,15 @@ void main() async {
     logger.i(result);
   });
 
-      test('Send transcript to OpenAI for Reminders', () async {
+  test('Send transcript to OpenAI for Reminders', () async {
     final conversation = TestConversations.sampleConversations.firstWhereOrNull(
         (convo) => convo.id == 'e3bc7acc-3b20-4056-94b6-6199fdba5870');
     final gpt = GptCalls(EnvironmentVars.openAIApiKey);
     expect(conversation, isNotNull);
     expect(conversation!.transcript, isNotEmpty);
 
-    String? result =
-        await gpt.getReminders(conversation.transcript, 'User profile.', conversation.recordedDate);
+    String? result = await gpt.getReminders(
+        conversation.transcript, 'User profile.', conversation.recordedDate);
     logger.i(result);
   });
 }
